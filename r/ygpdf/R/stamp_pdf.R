ae2d22900a036a4d012d790a9021b5bf5bfb884a
Yannick gouedard <yannick.gouedard@gmail.com>
2020-02-06 19:17:14 +0300

    (1) added transparency/opacity for both text and image. (2) I now use \documentclass{minimal} which allows me not to deal with papersize and use large paper like a0 as I define the pape size in px using the values pdftools gives me. (3) adding support of PDF filenames which include white spaces --> LaTeX do not like them. Beware just that if the filename includes a full or relative path, the path shall exist without the white spaces. [ TO DO ] see what happens if the stamp filename includes white spaces.

--- r/ygpdf/R/stamp_pdf.R
+++ r/ygpdf/R/stamp_pdf.R
@@ -6,67 +6,76 @@
 #' @keywords pdf, adding image, adding text
 #' @export
 #' @importFrom pdftools pdf_pagesize
-#' @importFrom knitr knit2pdf
+#' @import knitr
 #' @examples
 #'
-#' stamp_file <- 'logo_conspel.png'
-#' original_pdf_no_spaces <- gsub(' ', '', original_pdf)           # I do not want any spaces in pdf name
-#'                                                                 # so beware if original_pdf has got a path
-#' stamp_list <- data.frame(what = c(stamp_file, stamp_file),      # what?
-#'                          page = c(1, 2),                        # on wich page?
-#'                          type = c('image', 'image'),            # image or text?
-#'                          position_x = c(0.1, 0.5),              # ratio of page width
-#'                          position_y = c(0.2, 0.5),              # ratio of page height
-#'                          size = c(0.1, 0.5),                    # ratio of page width
-#'                          angle = c(0, 0),
-#'                          stringsAsFactors = FALSE)
-#' stamp_pdf(original_pdf_no_spaces, stamp_list, NULL)
+#' stamp_file <- system.file("images", "Rlogo.pdf", package = "ygpdf")
 #'
-#' Note that:   if (is.null(out_file)) { out_file <- gsub('.pdf', '-stamped.pdf', in_file) }
+#' stamp_list <- data.frame(what = c(stamp_file, stamp_file),  # what?
+#'                          page = c(1, 2),                    # on wich page?
+#'                          type = c('image', 'image'),        # image or text?
+#'                          position_x = c(0.1, 0.5),          # ratio of page width
+#'                          position_y = c(0.2, 0.5),          # ratio of page height
+#'                          size = c(0.1, 0.5),                # ratio of page width
+#'                          angle = c(0, 0),                   # rotation angle
+#'                          alpha = c(0.5, 0.5),               # transparency
+#'                          stringsAsFactors = FALSE)
+#' stamp_pdf(original_pdf, stamp_list, NULL)
+#'
+#' Note that:   if (is.null(out_file)) { out_file <- gsub('.pdf', ' - stamped.pdf', in_file) }
 #'
 
-stamp_pdf <- function(in_file, this_list = NULL, out_file = NULL) {
+stamp_pdf <- function(in_file, in_list = NULL, out_file = NULL) {
   # check for NULL entries and put some default
-  if (is.null(this_list)) {
-    this_list <- data.frame(what = c(system.file("images",
-                                                 "Rlogo.pdf",
-                                                 package = "ygpdf"),
-                                     'R logo'),
-                            page = c(1, 1),
-                            type = c('image', 'text'),
-                            position_x = c(0.5, 0.5),
-                            position_y = c(0.5, 0.75),
-                            size = c(0.5, 0.5),
-                            angle = c(15, 15),
-                            stringsAsFactors = FALSE)
+  if (is.null(in_list)) {
+    in_list <- data.frame(what = c(system.file("images",
+                                               "Rlogo.pdf",
+                                               package = "ygpdf"),
+                                    'R logo'),
+                          page = c(1, 1),
+                          type = c('image', 'text'),
+                          position_x = c(0.5, 0.5),
+                          position_y = c(0.5, 0.75),
+                          size = c(0.5, 0.5),
+                          angle = c(15, 15),
+                          alpha = c(0.5, 0.5),
+                          stringsAsFactors = FALSE)
   }
   if (is.null(out_file)) {
-    out_file <- gsub('.pdf', '-stamped.pdf', in_file)
+    out_file <- gsub('.pdf', ' - stamped.pdf', in_file)
   }
-  pagesize <- pdf_pagesize(in_file)
-  tex_file <- gsub('.pdf', '.tex', out_file)
-  this_paper <- 'unknown_paper'
   #
-  # check page size and orientation of the input PDF
-  # works only if all pages of the PDF have same papersize and orientation
+  # dealing with spaces in file names
   #
-  # [TODO]: see if there is a generic way to do this
-  #
-  if (pagesize[1, 5] == '841.89' & pagesize[1, 6] == '595.276') {
-    this_paper <- 'a4paper,landscape'
-  } else {
-    if (pagesize[1, 5] == '595.276' & pagesize[1, 6] == '841.89') {
-      this_paper <- 'a4paper'
-    }
+  in_file_no_spaces <- gsub(' ', '', in_file)
+  out_file_no_spaces <- gsub(' ', '', out_file)
+  remove_in_file_no_spaces_afterwards <- FALSE
+  remove_out_file_no_spaces_afterwards <- FALSE
+  if (in_file_no_spaces != in_file) {
+    file.copy(in_file, in_file_no_spaces)
+    remove_in_file_no_spaces_afterwards <- TRUE
   }
+  if (out_file_no_spaces != out_file) {
+    remove_out_file_no_spaces_afterwards <- TRUE
+  }
+
+  pagesize <- pdf_pagesize(in_file_no_spaces)
+  tex_file <- gsub('.pdf', '.tex', out_file_no_spaces)
 
   ##### create the rnw file
   #
   # I put an inline rnw as I do not want to have a second file (the .rnw)
   # perhaps there is a way to knit inline without the rnw file ...
   #
+  # I use \\documentclass{minimal}
+  # and define custom paper size as the one pdftools told me
+  # also defining \\paperwidth and \\paperheight
+  #
   stamped_rnw <- paste0(
-    '\\documentclass[', this_paper, ']{article}\n',
+    '\\documentclass{minimal}\n',
+    '\\special{papersize=', pagesize[1, 6], 'px,', pagesize[1, 5], 'px}',
+    '\\setlength{\\paperwidth}{', pagesize[1, 6], 'px}',
+    '\\setlength{\\paperheight}{', pagesize[1, 5], 'px}',
     '\\usepackage{eso-pic}',
     '\\usepackage{graphicx}',
     '\\usepackage{changepage}',
@@ -82,27 +91,28 @@ stamp_pdf <- function(in_file, this_list = NULL, out_file = NULL) {
     #
     stamped_rnw <- paste0(stamped_rnw,
       '\\AddToShipoutPictureBG*{\\AtStockLowerLeft{\\includegraphics[page=',
-      this_page, ']{', in_file, '}}}\n')
+      this_page, ']{', in_file_no_spaces, '}}}\n')
     #
     # put the stamps if there is one (or more) on this page
     # one tikzpicture per page
     #
     # [TODO]: see if we can have one tikzpicture for all images on all pages...
     #
-    if (this_page %in% unique(this_list$page)) {
+    if (this_page %in% unique(in_list$page)) {
       stamped_rnw <- paste0(stamped_rnw,
                             '\\begin{tikzpicture}[remember picture,overlay]\n')
-      for (this_stamp_pos in seq_along(this_list$what[this_list$page == this_page])) {
-        what <- this_list$what[this_list$page == this_page][this_stamp_pos]
-        this_position_x <- this_list$position_x[this_list$page == this_page][this_stamp_pos]
-        this_position_y <- this_list$position_y[this_list$page == this_page][this_stamp_pos]
-        this_size <- this_list$size[this_list$page == this_page][this_stamp_pos]
-        this_angle <- this_list$angle[this_list$page == this_page][this_stamp_pos]
-        if (this_list$type[this_list$page == this_page][this_stamp_pos] == 'image') {
+      for (this_stamp_pos in seq_along(in_list$what[in_list$page == this_page])) {
+        what <- in_list$what[in_list$page == this_page][this_stamp_pos]
+        this_position_x <- in_list$position_x[in_list$page == this_page][this_stamp_pos]
+        this_position_y <- in_list$position_y[in_list$page == this_page][this_stamp_pos]
+        this_size <- in_list$size[in_list$page == this_page][this_stamp_pos]
+        this_angle <- in_list$angle[in_list$page == this_page][this_stamp_pos]
+        this_alpha <- in_list$alpha[in_list$page == this_page][this_stamp_pos]
+        if (in_list$type[in_list$page == this_page][this_stamp_pos] == 'image') {
           stamped_rnw <- paste(stamped_rnw,
                                paste0('\\node [xshift=', this_position_x, '\\paperwidth,',
                                       'yshift=-', this_position_y, '\\paperheight,',
-                                      'rotate=', this_angle, ']',
+                                      'rotate=', this_angle, ',opacity=', this_alpha, ']',
                                       'at (current page.north west) ',
                                       '{\\includegraphics[width=', this_size,
                                       '\\paperwidth]{', what, '}} ;\n'))
@@ -110,7 +120,8 @@ stamp_pdf <- function(in_file, this_list = NULL, out_file = NULL) {
           stamped_rnw <- paste(stamped_rnw,
                                paste0('\\node [align=center,xshift=', this_position_x, '\\paperwidth,',
                                       'yshift=-', this_position_y, '\\paperheight,',
-                                      'rotate=', this_angle, ',scale=', this_size/30, '\\paperwidth,text opacity=0.4] ',
+                                      'rotate=', this_angle, ',scale=', this_size/30, '\\paperwidth,',
+                                      'text opacity=', this_alpha, '] ',
                                       'at (current page.north west) ',
                                       '{', what, '} ;\n'))
         }
@@ -132,8 +143,16 @@ stamp_pdf <- function(in_file, this_list = NULL, out_file = NULL) {
   #
   knit2pdf(input = rnw_file, output = tex_file)
   #
-  # remove temporary rnw file
+  # remove temporary files
   #
   file.remove(rnw_file)
+  file.remove(tex_file)
+  if (remove_in_file_no_spaces_afterwards) {
+    file.remove(in_file_no_spaces)
+  }
+  if (remove_out_file_no_spaces_afterwards) {
+    file.rename(out_file_no_spaces, out_file)
+  }
+
 }
 
